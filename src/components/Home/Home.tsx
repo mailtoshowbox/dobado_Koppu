e8ec1e4944fb43be0b1653a2f340ddcc2348bc07
showbox <mailtoshowbox@yahoo.com>
2020-12-18 18:48:52 +0530

    bliank  home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -22,46 +22,7 @@ const Home: React.FC = () => {
 
   return (
     <Fragment>
-      <h1 className="h3 mb-2 text-gray-800">Dashboard</h1>
-      <p className="mb-4">Summary and overview of our admin stuff here</p>
-
-      <div className="row">
-        <TopCard title="PRODUCT COUNT" text={`${numberItemsCount}`} icon="box" class="primary" />
-        <TopCard title="PRODUCT AMOUNT" text={`${totalProductAmount}`} icon="warehouse" class="danger" />
-        <TopCard title="SUMMARY PRICE" text={`$${totalPrice}`} icon="dollar-sign" class="success" />
-      </div>
-
-      <div className="row">
-        <TopCard title="SALES" text={totalSales.toString()} icon="donate" class="primary" />
-        <TopCard title="ORDER AMOUNT" text={totalOrderAmount.toString()} icon="calculator" class="danger" />
-      </div>
-
-      <div className="row">
-
-        <div className="col-xl-6 col-lg-6">
-          <div className="card shadow mb-4">
-            <div className="card-header py-3">
-              <h6 className="m-0 font-weight-bold text-green">Product list</h6>
-            </div>
-            <div className="card-body">
-              <ProductList />
-            </div>
-          </div>
-
-        </div>
-
-        <div className="col-xl-6 col-lg-6">
-          <div className="card shadow mb-4">
-            <div className="card-header py-3">
-              <h6 className="m-0 font-weight-bold text-green">Order list</h6>
-            </div>
-            <div className="card-body">
-              <OrderList />
-            </div>
-          </div>
-        </div>
-
-      </div>
+     
 
     </Fragment>
   );
